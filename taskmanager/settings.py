1d711488c956cbc567df2906ed50c5f59b82662b
Hubert Sidorowicz <148703988+hubertsid@users.noreply.github.com>
2023-11-07 16:54:10 +0100

    changes

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(__+#puloq+k&$4#z48fhjld6cv_f)3i3n)i0qmr9%le5!fedr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
