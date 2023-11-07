8aab5a237a321c802a674faef1bbe76843d7362b
Hubert Sidorowicz <148703988+hubertsid@users.noreply.github.com>
2023-11-07 17:53:58 +0100

    commit

--- main/models.py
+++ main/models.py
@@ -9,29 +9,30 @@ class Specialization(models.TextChoices):
     UX_UI = 'UX/UI', 'UX/UI'
 
 class Developer(models.Model):
-    #developers nick
-    id = models.CharField(max_length=255,primary_key=True)
-    #developers name
+    # Developer's nick
+    id = models.CharField(max_length=255, primary_key=True)
+    # Developer's name
     name = models.CharField(max_length=255)
-    #developers specialization
+    # Developer's specialization
     specialization = models.CharField(
         max_length=100,
         choices=Specialization.choices,
         default=Specialization.FRONTEND
     )
+
     class Meta:
         ordering = ["id"]
 
     def __str__(self):
         return self.id
-    
+
 class State(models.TextChoices):
     TO_DO = 'TO_DO', 'To-do'
     IN_PROGRESS = 'IN_PROGRESS', 'In progress'
     COMPLETED = 'COMPLETED', 'Completed'
 
 class Task(models.Model):
-    name = models.CharField(max_length=100,default="Unnamed")
+    name = models.CharField(max_length=100, default="Unnamed")
     project_id = models.CharField(max_length=100)
     credentials = models.CharField(
         max_length=100,
@@ -48,19 +49,18 @@ class Task(models.Model):
     created_at = models.DateTimeField()
     created_by = models.CharField()
 
-    class Meta():
+    class Meta:
         ordering = ["id"]
 
     def __str__(self):
         return str(self.id)
-    
 
 class Project(models.Model):
-    # nazwa projektu
+    # Project name
     id = models.CharField(max_length=100, primary_key=True)
-    # nazwa twórcy projektu
+    # Project creator's name
     user = models.CharField(max_length=100)
-    # lista deweloperów w projekcie
+    # List of developers in the project
     developers = models.ManyToManyField(Developer, default=None)
-    # lista tasków w projekcie
+    # List of tasks in the project
     tasks = models.ManyToManyField(Task, default=None)
